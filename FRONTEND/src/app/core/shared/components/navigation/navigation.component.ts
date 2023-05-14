acb51bcda44870752f11405cba19095020b416ac
Moleiro <alberto.moleiro7@gmail.com>
2023-05-14 17:04:59 +0200

    Frontend/ Feature - Theme colors changed

--- FRONTEND/src/app/core/shared/components/navigation/navigation.component.ts
+++ FRONTEND/src/app/core/shared/components/navigation/navigation.component.ts
@@ -12,6 +12,7 @@ import { NavLink } from 'src/app/core/models/interfaces/nav-link.interface';
 })
 export class NavigationComponent {
     mode: MatDrawerMode = 'side';
+    opened: boolean = true;
     links: NavLink[] = [
         { icon: 'home', name: 'Inicio', route: '/registered/home' },
         { icon: 'map', name: 'Crear Ruta', route: '/registered/route/creator' },
@@ -20,12 +21,13 @@ export class NavigationComponent {
         { icon: 'exit_to_app', name: 'Cerrar sesión', route: '/login' }
     ];
     constructor(private breakpointObserver:BreakpointObserver) {}
-    
+
     ngOnInit(): void {
         this.breakpointObserver
         .observe(['(min-width: 600px)'])
         .subscribe((state: BreakpointState) => {
           this.mode = state.matches ? 'side' : 'over';
+            this.opened = state.matches;
         });
     }
 }
