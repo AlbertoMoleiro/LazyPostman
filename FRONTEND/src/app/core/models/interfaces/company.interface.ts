67dd597355f86c927e579aeb921f7d9e05a9f541
Moleiro <alberto.moleiro7@gmail.com>
2023-05-15 17:48:02 +0200

    Frontend / Feature - Created User and Company interfaces

---
+++ FRONTEND/src/app/core/models/interfaces/company.interface.ts
@@ -0,0 +1,10 @@
+export interface Company {
+    CIF: string;
+    businessName: string;
+    address: string;
+    province: string;
+    postCode: string;
+    phoneNumber: string;
+    email: string;
+    password: string;
+}
