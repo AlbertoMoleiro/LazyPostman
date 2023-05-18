da5a8afc1a55cf9f22b9724e495f16c5ed0e27a5
AlbertoMoleiro <alberto.moleiro@softtek.com>
2023-05-18 12:27:16 +0200

    Frontend/Feature - Route View: google map setted and api lazy loaded

--- FRONTEND/src/app/registered/optimized-routes/optimized-routes.module.ts
+++ FRONTEND/src/app/registered/optimized-routes/optimized-routes.module.ts
@@ -6,18 +6,21 @@ import { OptimizedRoutesRoutingModule } from './optimized-routes-routing.module'
 import { RouteCreatorComponent } from './route-creator/containers/route-creator.component';
 import { RouteViewComponent } from './route-view/containers/route-view.component';
 import { ItineraryComponent } from './itinerary/containers/itinerary.component';
-
-
+import { GoogleMapsModule } from '@angular/google-maps';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    RouteCreatorComponent,
-    RouteViewComponent,
-    ItineraryComponent
-  ],
-  imports: [
-    CommonModule,
-    OptimizedRoutesRoutingModule
-  ]
+    declarations: [
+        RouteCreatorComponent,
+        RouteViewComponent,
+        ItineraryComponent
+    ],
+    imports: [
+        CommonModule,
+        GoogleMapsModule,
+        HttpClientModule,
+        HttpClientJsonpModule,
+        OptimizedRoutesRoutingModule
+    ]
 })
 export class OptimizedRoutesModule { }
--- FRONTEND/src/app/registered/optimized-routes/route-view/containers/route-view.component.ts
+++ FRONTEND/src/app/registered/optimized-routes/route-view/containers/route-view.component.ts
@@ -1,10 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable, catchError, map, of } from 'rxjs';
+
 
 @Component({
-  selector: 'app-route-view',
-  templateUrl: './route-view.component.html',
-  styleUrls: ['./route-view.component.css']
+    selector: 'app-route-view',
+    templateUrl: './route-view.component.html',
+    styleUrls: ['./route-view.component.css']
 })
 export class RouteViewComponent {
-
+    apiLoaded: Observable<boolean>;
+    constructor(httpClient: HttpClient) {
+        this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyALc0EyqPY2yBRmX7ri9UQ2mrSycg7tmoY', 'callback')
+            .pipe(
+              map(() => true),
+              catchError(() => of(false)),
+            );
+      }
 }
