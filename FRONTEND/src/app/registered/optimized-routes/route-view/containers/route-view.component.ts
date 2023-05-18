706113147acbe722c036960b3a1277b9534fb38b
AlbertoMoleiro <alberto.moleiro@softtek.com>
2023-05-18 16:52:03 +0200

    Frontend / Feature - RouteView component front done

--- FRONTEND/src/app/registered/optimized-routes/route-view/containers/route-view.component.ts
+++ FRONTEND/src/app/registered/optimized-routes/route-view/containers/route-view.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable, catchError, map, of } from 'rxjs';
+import { MapDirectionsService } from '@angular/google-maps';
+import { Observable, map } from 'rxjs';
 
 
 @Component({
@@ -9,12 +9,35 @@ import { Observable, catchError, map, of } from 'rxjs';
     styleUrls: ['./route-view.component.css']
 })
 export class RouteViewComponent {
-    apiLoaded: Observable<boolean>;
-    constructor(httpClient: HttpClient) {
-        this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyALc0EyqPY2yBRmX7ri9UQ2mrSycg7tmoY', 'callback')
-            .pipe(
-              map(() => true),
-              catchError(() => of(false)),
-            );
-      }
+    apiLoaded: Observable<boolean> = new Observable<boolean>();
+
+    readonly directionsResults$: Observable<google.maps.DirectionsResult | undefined>;
+    center: google.maps.LatLngLiteral = { lat: 24, lng: 12 };
+    zoom = 6;
+
+    waypoints: google.maps.DirectionsWaypoint[] = [
+        { location: { lat: 40.47926648979756, lng: -3.85230427824624 }, stopover: true },
+        { location: { lat: 40.476938110090124, lng: -3.8533896919235113 }, stopover: true },
+        { location: { lat: 40.47538294425428, lng: -3.8580386572206886 }, stopover: true },
+        { location: { lat: 40.478236687937795, lng: -3.8556799626623883 }, stopover: true },
+        { location: { lat: 40.479896606829776, lng: -3.8600259038992633 }, stopover: true },
+
+    ];
+
+    constructor(mapDirectionsService: MapDirectionsService) {
+        const request: google.maps.DirectionsRequest = {
+            destination: { lat: 40.481450490571746, lng: -3.855249568664476 },
+            origin: { lat: 40.481450490571746, lng: -3.855249568664476 },
+            optimizeWaypoints: true,
+            travelMode: google.maps.TravelMode.DRIVING,
+            waypoints: this.waypoints
+        };
+        this.directionsResults$ = mapDirectionsService.route(request).pipe(map(response => response.result));
+
+    }
+    ngOnInit(): void {
+
+
+    }
+
 }
