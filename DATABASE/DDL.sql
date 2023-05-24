e97f5e3e784d2f5498c483e76ff78334f2e242a0
IsraelSerrano1 <israel.serrano@softtek.com>
2023-05-24 16:49:41 +0200

    Database / Feature Add column Lastname2

--- DATABASE/DDL.sql
+++ DATABASE/DDL.sql
@@ -49,7 +49,8 @@ CREATE TABLE ROUTES (
 CREATE TABLE USERS (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
+    lastname1 VARCHAR(255) NOT NULL,
+	lastname2 VARCHAR(255),
     phone_number VARCHAR(255),
     login VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
