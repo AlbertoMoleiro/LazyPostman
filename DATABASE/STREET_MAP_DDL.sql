84d708a45d82c0848eb8cda55d9f3a54b6909e22
AlbertoMoleiro <alberto.moleiro@softtek.com>
2023-06-01 15:33:12 +0200

    Database / Feature - Post code table created and added madrid post codes, add files with the data

--- DATABASE/STREET_MAP_DDL.sql
+++ DATABASE/STREET_MAP_DDL.sql
@@ -10,12 +10,36 @@ CREATE TABLE madrid_streets (
     COORD_X real,
     COORD_Y real
 );
-
+-- La carpeta cargadatos debe tener permisos
 COPY madrid_streets (CDMUNI, DSMUNI, CDTVIA, DSPART, DSVIAL, DSVIAL_NOR, NUMERO, COORD_X, COORD_Y)
 FROM 'C:\cargadatos\callejerov1.csv' 
 DELIMITER ';' 
 CSV HEADER
 ENCODING 'windows-1251';
+
+CREATE TABLE postal_codes(
+	id serial PRIMARY KEY,
+	post_code INT,
+	cdmuni INT,
+	dsmuni VARCHAR(200)
+)
+
+COPY postal_codes (post_code,cdmuni,dsmuni)
+FROM 'C:\cargadatos\POSTAL_CODES.csv' 
+DELIMITER ',' 
+CSV HEADER
+ENCODING 'windows-1251';
+
+--Procedimiento para que cuadren los codigos de municipio de las dos tablas
+CREATE OR REPLACE PROCEDURE update_last_three_digits()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+   UPDATE postal_codes SET cdmuni = cdmuni % 1000;
+END;
+$$;
+
+CALL update_last_three_digits();
 
 
 
