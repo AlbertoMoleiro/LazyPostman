916e3b0de1d624ddea9b8f3208f078495a1a5bc3
AlbertoMoleiro <alberto.moleiro@softtek.com>
2023-06-01 16:52:15 +0200

    Backend / Feature - Postal codes added to response of /route/towns

--- BACKEND/optimize-route/src/main/java/com/lazypostman/optimizeroute/controller/RouteController.java
+++ BACKEND/optimize-route/src/main/java/com/lazypostman/optimizeroute/controller/RouteController.java
@@ -1,10 +1,12 @@
 package com.lazypostman.optimizeroute.controller;
 
 import com.lazypostman.optimizeroute.dto.RequestRoadDTO;
+import com.lazypostman.optimizeroute.dto.TownDTO;
 import com.lazypostman.optimizeroute.model.formcreator.Road;
 import com.lazypostman.optimizeroute.model.formcreator.Town;
 import com.lazypostman.optimizeroute.model.requestroute.RequestRoad;
 import com.lazypostman.optimizeroute.model.requestroute.Waypoint;
+import com.lazypostman.optimizeroute.service.IPostalCodesService;
 import com.lazypostman.optimizeroute.service.MadridStreetsService;
 import com.lazypostman.optimizeroute.service.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/route")
 public class RouteController {
 
@@ -23,9 +28,21 @@ public class RouteController {
     private MadridStreetsService madridStreetsService;
     @Autowired
     private RouteService routeService;
+    @Autowired
+    private IPostalCodesService postalCodesService;
     @GetMapping("/towns")
-    public ResponseEntity<Set<Town>> getTowns() throws Exception {
-        return new ResponseEntity<>(madridStreetsService.getTowns(), HttpStatus.OK);
+    public ResponseEntity<Set<TownDTO>> getTowns() throws Exception {
+        Set<TownDTO> towns = madridStreetsService.getTowns().stream().map(town -> {
+            List<Integer> postalCodes;
+            try {
+                postalCodes = postalCodesService.findByCdmuni(town.getCdmuni());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return new TownDTO(town.getCdmuni(), town.getDsmuni(), postalCodes);
+        }).collect(Collectors.toSet());
+        return new ResponseEntity<>(towns, HttpStatus.OK);
+
     }
 
     @GetMapping("/roads/{cdmuni}")
