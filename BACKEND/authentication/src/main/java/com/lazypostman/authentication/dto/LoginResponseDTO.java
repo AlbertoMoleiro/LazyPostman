7c5263ce5aa6e3e39976adf403d11aefb0416d74
AlbertoMoleiro <alberto.moleiro@softtek.com>
2023-06-02 12:54:50 +0200

    Backend / Feature - Auth microservice done.

---
+++ BACKEND/authentication/src/main/java/com/lazypostman/authentication/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.lazypostman.authentication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private Integer id;
+    private Integer role;
+}
